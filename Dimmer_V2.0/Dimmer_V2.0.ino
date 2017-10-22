cebe090d7bbb86f3b1b60b26be32f8866aa9c2a0
Pedro Benevides <32526712+PedroUnifor@users.noreply.github.com>
2017-10-22 17:39:36 -0300

    Update Dimmer_V2.0.ino

--- Dimmer_V2.0/Dimmer_V2.0.ino
+++ Dimmer_V2.0/Dimmer_V2.0.ino
@@ -9,19 +9,13 @@
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(10,11);   //Inicia o software serial nos pinos 10 e 11
-
+SoftwareSerial mySerial(10,11);                                     //Inicia o software serial nos pinos 10 e 11
 //Pinos
 const int led0 = 3,    led1 = 5,    led2 = 6,    led3 = 9;
 const int ldr0 = A0,   ldr1 = A1,   ldr2 = A2,   ldr3 = A3;
-
 //Variaveis do PID
-float kp0 = 0.3,  ki0 = 0.009,  kd0 = 0.006,  i0;
-float kp1 = 0.3,  ki1 = 0.009,  kd1 = 0.006,  i1;
-float kp2 = 0.3,  ki2 = 0.009,  kd2 = 0.006,  i2;
-float kp3 = 0.3,  ki3 = 0.009,  kd3 = 0.006,  i3;
-
-int setPoint0 = 85, setPoint1 = 85, setPoint2 = 85, setPoint3 = 85;     //O setPoint de luminosidade sempre começa em 85%
+float kp = 0.3,  ki = 0.009,  kd = 0.006,  i0, i1, i2, i3;
+int setPoint = 85;                                                  //O setPoint de luminosidade sempre começa em 85% ???
 int erro0, erro1, erro2, erro3;
 int vAtual0, vAtual1, vAtual2, vAtual3;
 unsigned long tAtual0 = 0, tAtual1 = 0, tAtual2 = 0, tAtual3 = 0;
@@ -36,53 +30,25 @@ void setup() {
 }
 
 void loop() {
-  //  Leitura dos dados via bluetooth
-  //  O app envia o ID do objeto mais o valor. EX: setar o slider do led1 para 68% envia a string "L060"
-  String serialRead = "";
-  while(mySerial.available())
-  {
-    char c = mySerial.read();
-    serialRead += c;
-    delay(2);
-  }
-  if(serialRead.length() > 0)
-  {
-    if(serialRead.substring(0,2) == "L0")
-    {
-      setPoint0 = serialRead.substring(2).toInt();
-    }
-    if(serialRead.substring(0,2) == "L1")
-    {
-      setPoint1 = serialRead.substring(2).toInt();
-    }
-    if(serialRead.substring(0,2) == "L2")
-    {
-      setPoint2 = serialRead.substring(2).toInt();
-    }
-    if(serialRead.substring(0,2) == "L3")
-    {
-      setPoint3 = serialRead.substring(2).toInt();
-    } 
-  }
-
+  
+  getSerial();        //Lê o bluetooth
+  
   //Calculo do PID e escrita do pid nos pinos
   int ledRead0 = map(analogRead(ldr0), 1023, 7, 0, 100);
-  float pid0 = getPID(ledRead0, led0, setPoint0);
+  float pid0 = getPID(ledRead0, led0);
   analogWrite(led0, pid0);
-  
   int ledRead1 = map(analogRead(ldr0), 1023, 7, 0, 100)/2 + map(analogRead(ldr1), 1020, 5, 0, 100)/2;
-  float pid1 = getPID(ledRead1, led1, (setPoint0/2 + setPoint1/2));
+  float pid1 = getPID(ledRead1, led1); 
   analogWrite(led1, pid1);
-  
   int ledRead2 = map(analogRead(ldr1), 1020, 5, 0, 100)/2 + map(analogRead(ldr2), 1023, 10, 0, 100)/2;
-  float pid2 = getPID(ledRead2, led2, (setPoint1/2 + setPoint2/2));
+  float pid2 = getPID(ledRead2, led2);
   analogWrite(led2, pid2);
-  
   int ledRead3 = map(analogRead(ldr2), 1023, 10, 0, 100);
-  float pid3 = getPID(ledRead3, led3, setPoint2);
+  float pid3 = getPID(ledRead3, led3);
   analogWrite(led3, pid3);
 
-  if(millis() - lastTime > 300){
+//Dados para o monitor serial
+  if(millis() - lastTime > 1000){
     lastTime = millis();
     int ldrr0 = map(analogRead(ldr0), 1023, 7, 0, 100);
     int ldrr1 = map(analogRead(ldr1), 1020, 5, 0, 100);
@@ -95,16 +61,16 @@ void loop() {
     Serial.println(msgPID + "\t" + msgLDR);
   }
 }
+
 /* 
  *  A função getPID retorna o controlador PID, que está limitado a um valor entre 0 e 255.
  *  O switchcase analisa qual LDR e setPoint foram passados na função e executa o PID com base nisso
  */
-float getPID(int ledRead, const int &LED, int setPoint)
+float getPID(int ledRead, const int &LED)
 {
   int *valorAtual, *erro;
   float *i;
   unsigned long *tempoAtual;
-  float *kp, *ki, *kd;
 
   switch (LED)
   {
@@ -114,9 +80,6 @@ float getPID(int ledRead, const int &LED, int setPoint)
       erro = &erro0;
       tempoAtual = &tAtual0;
       i = &i0;
-      kp = &kp0;
-      ki = &ki0;
-      kd = &kd0;
     }break;
     case led1:
     {
@@ -124,9 +87,6 @@ float getPID(int ledRead, const int &LED, int setPoint)
       erro = &erro1;
       tempoAtual = &tAtual1;
       i = &i1;
-      kp = &kp1;
-      ki = &ki1;
-      kd = &kd1;
     }break;
     case led2:
     {
@@ -134,9 +94,6 @@ float getPID(int ledRead, const int &LED, int setPoint)
       erro = &erro2;
       tempoAtual = &tAtual2;
       i = &i2;
-      kp = &kp2;
-      ki = &ki2;
-      kd = &kd2;
     }break;
     case led3:
     {
@@ -144,9 +101,6 @@ float getPID(int ledRead, const int &LED, int setPoint)
       erro = &erro3;
       tempoAtual = &tAtual3;
       i = &i3;
-      kp = &kp3;
-      ki = &ki3;
-      kd = &kd3;
     }break;
   }
 
@@ -160,11 +114,11 @@ float getPID(int ledRead, const int &LED, int setPoint)
   *tempoAtual = millis();
   double dt = *tempoAtual - tempoAnterior;
 
-  float p = *erro * (*kp);                  // P = kp*E(t)
-  *i += *erro * (*ki) * dt;                 // i = ki*SE(t)*dt
+  float p = *erro * kp;                  // P = kp*E(t)
+  *i += *erro * ki * dt;                 // i = ki*SE(t)*dt
   if(*i > 200) { *i = 200; }                // i está limitado a -50 e 200
   if(*i < -50)   { *i = -50; }
-  float d = (*valorAtual - valorAnterior) * dt * (*kd);     // d = kd*(dE(t)/d(t))
+  float d = (*valorAtual - valorAnterior) * dt * kd;     // d = kd*(dE(t)/d(t))
   float PID = 50+ p + *i + d;
   if(PID > 255) { PID = 255; }
   if(PID < 0) { PID = 0; }
@@ -172,3 +126,30 @@ float getPID(int ledRead, const int &LED, int setPoint)
   return PID;
 }
 
+/* 
+ *  Leitura dos dados via bluetooth
+ *  O app envia o ID do objeto mais o valor. EX: setar o slider da sala0 para 68% envia a string "L060"
+ */
+void getSerial()
+{
+  //  Leitura dos dados via bluetooth
+  //  O app envia o ID do objeto mais o valor. EX: setar o slider da sala0 para 68% envia a string "L060"
+  String serialRead = "";
+  while(mySerial.available())
+  {
+    char c = mySerial.read();
+    serialRead += c;
+    delay(2);
+  }
+  if(serialRead.length() > 0)
+  {
+    String strCmp = serialRead.substring(0,2);
+    if(strCmp == "L0") { setPoint = serialRead.substring(2).toInt(); }
+    if(strCmp == "p+") { kp += 0.1; }
+    if(strCmp == "p-") { kp -= 0.1; }
+    if(strCmp == "i+") { ki += 0.001; }
+    if(strCmp == "i-") { ki -= 0.001; }
+    if(strCmp == "d+") { kd += 0.001; }
+    if(strCmp == "d-") { kd -= 0.001; }
+  }
+}
