7085414302b1bf2876933ec09c812fa0a42ed872
lucasbh <lucashbezerra@hotmail.com>
2017-11-29 02:23:13 -0300

    Melhorias PID
    Sample time, derivative kick, pid tuning changes, man/auto,

--- Dimmer_V2.0/Dimmer_V2.0.ino
+++ Dimmer_V2.0/Dimmer_V2.0.ino
@@ -14,13 +14,15 @@ const int led0 = 3,    led1 = 5,    led2 = 6,    led3 = 9;
 const int ldr0 = A0,   ldr1 = A1,   ldr2 = A2,   ldr3 = A3;
 
 //Variaveis do PID
-float kp = 0.3,  ki = 0.009,  kd = 0.006,  i0, i1, i2, i3;
+float kp = 0.3,  ki = 0.09,  kd = 0.06,  i0, i1, i2, i3;
 int setPoint = 85;                                                  //O setPoint de luminosidade sempre começa em 85% ???
 int erro0, erro1, erro2, erro3;
-int vAtual0, vAtual1, vAtual2, vAtual3;
-unsigned long tAtual0 = 0, tAtual1 = 0, tAtual2 = 0, tAtual3 = 0;
+int lastValor0, lastValor1, lastValor2, lastValor3;
+unsigned long lastT0 = 0, lastT1 = 0, lastT2 = 0, lastT3 = 0;
 unsigned long lastTime = 0;
+int tAmostragem = 100;     // Tempo de amostragem = 0.2s
 float pid0, pid1, pid2, pid3;
+int pwmMin = 0, pwmMax = 255; 
 
 //Variaveis extras
 int ldrRead0, ldrRead1, ldrRead2;
@@ -55,16 +57,16 @@ void loop() {
     ldrRead2 = map(analogRead(ldr2), 1023, 7, 0, 100);
     
     pid0 = getPID(ldrRead0, led0);
-    analogWrite(led0, pid0);
+    if(pid0 != -1){analogWrite(led0, pid0);}
     
     pid1 = getPID((ldrRead0/2 + ldrRead1/2), led1); 
-    analogWrite(led1, pid1);
+    if(pid1 != -1){analogWrite(led1, pid1);}
     
     pid2 = getPID(ldrRead1/2 + ldrRead2/2, led2);
-    analogWrite(led2, pid2);
+    if(pid2 != -1){analogWrite(led2, pid2);}
     
     pid3 = getPID(ldrRead2, led3);
-    analogWrite(led3, pid3);
+    if(pid3 != -1){analogWrite(led3, pid3);}
   
     sendSerial();
   }else                 // Se a flag estiver em manual (true)
@@ -78,70 +80,127 @@ void loop() {
 }
 
 
-float getPID(int ledRead, const int &LED)
+float getPID(int ldrRead, const int &LED)
 {
 //A função getPID retorna o controlador PID, que está limitado a um valor entre 0 e 255.
 //O switchcase analisa qual LDR e setPoint foram passados na função e executa o PID com base nisso
 
-  int *valorAtual, *erro;
-  float *i;
-  unsigned long *tempoAtual;
+  int *lastValor, *erro;
+  float *i;							//Soma dos erros
+  unsigned long *lastTempo;
 
   switch (LED)
-  {
-    case led0:
-    {
-      valorAtual = &vAtual0;
-      erro = &erro0;
-      tempoAtual = &tAtual0;
-      i = &i0;
-    }break;
-    case led1:
-    {
-      valorAtual = &vAtual1;
-      erro = &erro1;
-      tempoAtual = &tAtual1;
-      i = &i1;
-    }break;
-    case led2:
-    {
-      valorAtual = &vAtual2;
-      erro = &erro2;
-      tempoAtual = &tAtual2;
-      i = &i2;
-    }break;
-    case led3:
-    {
-      valorAtual = &vAtual3;
-      erro = &erro3;
-      tempoAtual = &tAtual3;
-      i = &i3;
-    }break;
+	  {
+	    case led0:
+	    {
+	      lastValor = &lastValor0;
+	      erro = &erro0;
+	      lastTempo = &lastT0;
+	      i = &i0;
+	    }break;
+	    case led1:
+	    {
+	      lastValor = &lastValor1;
+	      erro = &erro1;
+	      lastTempo = &lastT1;
+	      i = &i1;
+	    }break;
+	    case led2:
+	    {
+	      lastValor = &lastValor2;
+	      erro = &erro2;
+	      lastTempo = &lastT2;
+	      i = &i2;
+	    }break;
+	    case led3:
+	    {
+	      lastValor = &lastValor3;
+	      erro = &erro3;
+	      lastTempo = &lastT3;
+	      i = &i3;
+	    }break;
   }
 
-  //Encontrando E(t) (erro)
-  int valorAnterior = *valorAtual;
-  *valorAtual = ledRead;
-  int lastErro = *erro;
-  *erro = setPoint - *valorAtual;
-
   //Encontrando dt
-  unsigned long tempoAnterior = *tempoAtual;
-  *tempoAtual = millis();
-  double dt = *tempoAtual - tempoAnterior;
+  //unsigned long tempoAnterior = *tempoAtual;
+  unsigned long Agora = millis();
+  double difTempo = Agora - *lastTempo;
+  if(difTempo>=tAmostragem)						     	 //Se a variação de tempo(dt) for maior que o tempo de amostragem, executa o codigo
+  {
+    //Encontrando E(t) (erro)
+    *erro = setPoint - ldrRead;							 //Erro
 
-  float p = *erro * kp;                                   // P = kp*E(t)
-  *i += *erro * ki * dt;                                  // i = ki*SE(t)*dt
-  if(*i > 200) { *i = 200; }                              // i está limitado a -50 e 200
-  if(*i < -50)   { *i = -50; }
-  float d = kd * ((*erro - lastErro) / dt);               // d = kd*(dE(t)/d(t))
-  
-  float PID = 50 + p + *i + d;
-  if(PID > 255) { PID = 255; }
-  if(PID < 0) { PID = 0; }
+    double dInput = ldrRead - *lastValor;				 // Melhoria Derivative Kick: Qualquer mudança no setPoint nao causara oscilação
+    													//dInput = input - lastInput
 
-  return PID;
+    float p = *erro * kp;                                // P = kp*E(t)
+
+    *i += *erro * ki;                              		 // i = ki*S(E(t)*dt)  --->  i = S(ki*E(t)*dt) Melhoria tuning changes
+    											  	     // Com a melhoria sample time, sabemos que o intervalo
+    													 // do tempo de execução é constante e dt não é mais necessário.
+    if(*i > pwmMax) { *i = pwmMax; }                  	 // a integração está limitada a 0 e 255
+    if(*i < pwmMin) { *i = pwmMin; }
+		 
+    											  	     // Com a melhoria sample time, sabemos que o intervalo
+    													 // do tempo de execução é constante e dt não é mais necessário.
+
+    float d = kd * dInput;               				 // d = kd*(dE(t)/d(t))			--> Com a melhoria
+
+    float PID = p + *i - d;							 // Com a melhoria derivative kick, o compensador se torna negativo. 
+    if(PID > pwmMax) { PID = pwmMax; }
+    if(PID < pwmMin) { PID = pwmMin; }
+
+    *lastValor = ldrRead;
+    *lastTempo = Agora;
+    return PID;
+  }else return -1;
 }
+
+void setAmostragem(int tNovo)
+{
+//Seta o tempo de amostragem
+  if(tNovo > 0)
+  {
+    double ratio = ((double)tNovo / (double)tAmostragem);
+    ki *= ratio;
+    kd /= ratio;
+    tAmostragem = (unsigned long)tNovo;
+  }
+}
+
+void setTunings(String strCmp, int ganho)
+{
+//Melhoria setTunings. 
+	if(strCmp == "kp") 
+	{
+		kp = (float)ganho / 1000; 
+	}
+	if(strCmp == "ki") 
+	{
+		int KI = (float)ganho / 1000; 
+		ki = KI * tAmostragem;
+	}
+	if(strCmp == "kp") 
+	{
+		int KD = (float)ganho / 1000;
+		kd = KD / tAmostragem;
+	}
+}
+
+
+/*void setOutLimit(double Min, double Max)
+{
+//Melhoria reset windup.
+   if(Min > Max) return;
+   pwmMin = Min;
+   pwmMax = Max;
+    
+   if(PID > pwmMax) PID = pwmMax;
+   else if(PID < pwmMin) PID = pwmMin;
+ 
+   if(i> outMax) i= outMax;
+   else if(i< outMin) i= outMin;
+}*/
 
 void getSerial()
 {
@@ -161,18 +220,24 @@ void getSerial()
 
     // atualiza o setPoint
     if(strCmp == "L0") { setPoint = serialRead.substring(2).toInt(); }
-
-    // setTunings: Altera os ganhos do controlador
-    if(strCmp == "p+") { kp = kp + 0.1; }        
-    if(strCmp == "p-") { kp -= 0.1; }
-    if(strCmp == "i+") { ki += 0.001; }
-    if(strCmp == "i-") { ki -= 0.001; }
-    if(strCmp == "d+") { kd += 0.001; }
-    if(strCmp == "d-") { kd -= 0.001; }
-
+    
     // on/off(Auto/Man): altera de manual para automatico.
     if(strCmp == "FA") { flagManAuto = 0; }
     if(strCmp == "FM") { flagManAuto = 1; }
+    
+    // setTunings: Altera os ganhos do controlador
+    if(strCmp == "kp" || "ki" || "kd")
+    {
+		int ganho = serialRead.substring(2).toInt();
+		setTunings(strCmp, ganho);
+	}
+
+	//setSampleTime
+	if(strCmp == "ST") 
+	{
+		int newAmostragem = serialRead.substring(2).toInt();
+		setAmostragem(newAmostragem); 
+	}
   }
 }
 
@@ -183,19 +248,13 @@ void sendSerial()
   if(millis() - lastTime > 1000){
     if(!flagManAuto){
       lastTime = millis();
-      String msgPID = String((float)pid0) + " " + String((float)pid1) + " " + String((float)pid2) + " " + String((float)pid3);
-      String msgLDR = "LDR 0: " + String((int)ldrRead0) + " LDR 1: " + String((int)ldrRead1) + " LDR 2: " + String((int)ldrRead2);
+      String msgPID = String((float)pid0) + " | " + String((float)pid1) + " | " + String((float)pid2) + " | " + String((float)pid3);
+      String msgLDR = "LDR 0: " + String((int)ldrRead0) + "| LDR 1: " + String((int)ldrRead1) + "| LDR 2: " + String((int)ldrRead2);
       Serial.println(msgPID + "\t" + msgLDR);
       
       mySerial.println("kp " + String((float)kp));
       mySerial.println("ki " + String((float)ki));
       mySerial.println("kd " + String((float)kd));
-      mySerial.println("M0");
-      mySerial.println("A1");
-    }
-    else{
-      mySerial.println("M1");
-      mySerial.println("A0");
     }
   }
 }
